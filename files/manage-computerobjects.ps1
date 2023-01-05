5a292fb7bebc25d6ce11c9c2b17425b6214696db
Sven Jansen <sven.jansen@devicetrust.com>
2023-01-05 14:05:16 +0100

    .

--- files/manage-computerobjects.ps1
+++ files/manage-computerobjects.ps1
@@ -9,17 +9,17 @@ function move-computer {
 }
 
 $ComputerObject = "citrix-ctrl-01"
-$TargetOU = "OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=democloud,DC=local"
+$TargetOU = "OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
 Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
 
 $ComputerObject = "citrix-rdsh-01"
-$TargetOU = "OU=RDSH,OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=democloud,DC=local"
+$TargetOU = "OU=RDSH,OU=Citrix,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
 Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
 
 $ComputerObject = "vmware-ctrl-01"
-$TargetOU = "OU=RDSH,OU=VMware,OU=03_Environment,OU=Computer,OU=Objects,DC=democloud,DC=local"
+$TargetOU = "OU=RDSH,OU=VMware,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
 #Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
 
 $ComputerObject = "vmware-rdsh-01"
-$TargetOU = "OU=RDSH,OU=VMware,OU=03_Environment,OU=Computer,OU=Objects,DC=democloud,DC=local"
+$TargetOU = "OU=RDSH,OU=VMware,OU=03_Environment,OU=Computer,OU=Objects,DC=dt,DC=democloud"
 #Move-computer -ComputerObject $ComputerObject -TargetOU $TargetOU
